081277b945d35717770fb8029a37007d4a0f4558
Lavanya N M <lavanyanm75@gmail.com>
2026-07-21 17:08:09 +0530

    logical operation

---
+++ logical_operation.java
@@ -0,0 +1,14 @@
+public class logical_operation {
+    public static void main(String arg[])
+    {
+        int x=4;
+        int y =10;
+        int a=13;
+        int b=7;
+        boolean result1= (x>y)&&(a<b);
+         boolean result2= (x>y)||(a<b);
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+}
