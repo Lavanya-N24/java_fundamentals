afc1a27654d6cd0ba1250761a1cecac288481f84
Lavanya N M <lavanyanm75@gmail.com>
2026-07-26 12:57:16 +0530

    all code

---
+++ Inheritance.java
@@ -0,0 +1,35 @@
+ class Calculator{
+    int n1,n2;
+    void add(){
+        System.out.println(n1+n2);
+     
+    }
+    void sub(){
+        System.out.println(n1-n2);
+    }
+
+}
+ class AdvancedCalculator extends Calculator{
+    void mul(){
+        System.out.println(n1*n2);
+    }
+    void div(){
+        System.out.println(n1/n2);
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        AdvancedCalculator ac=new AdvancedCalculator();
+        ac.n1=10;
+        ac.n2=5;
+         System.out.println("Addition");
+        ac.add();
+        System.out.println("Subtraction");
+        ac.sub();
+        System.out.println("Multiplication");
+        ac.mul();
+        System.out.println("Division");
+        ac.div();
+       
+    }
+}
